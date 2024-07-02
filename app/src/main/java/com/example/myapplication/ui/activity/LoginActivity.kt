43856291e46b0625b7693f03717c6280a4e983a5
Randy Steven <randysteven12@gmail.com>
2024-07-02 22:37:16 +0700

    login form

--- app/src/main/java/com/example/myapplication/ui/activity/LoginActivity.kt
+++ app/src/main/java/com/example/myapplication/ui/activity/LoginActivity.kt
@@ -1,8 +1,29 @@
 package com.example.myapplication.ui.activity
 
+import android.os.Bundle
+import android.os.PersistableBundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.example.myapplication.Greeting
+import com.example.myapplication.ui.compose.LoginForm
+import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class LoginActivity : ComponentActivity() {
 
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        setContent {
+            MyApplicationTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    LoginForm()
+                }
+            }
+        }
+    }
 
 }
--- app/src/main/java/com/example/myapplication/ui/compose/LoginForm.kt
+++ app/src/main/java/com/example/myapplication/ui/compose/LoginForm.kt
@@ -1,6 +1,13 @@
 package com.example.myapplication.ui.compose
 
+import androidx.compose.foundation.layout.Row
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 
+@Composable
 fun LoginForm() {
+    Row {
+        EmailInput(value = "Email", onChange = {} )
+        PasswordInput(value = "Password", onChange = {})
+    }
 }
